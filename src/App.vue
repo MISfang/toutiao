ec4be8c86a1240b5fbd8b30826ed7a6c6b80c6f9
MISfang <60245677+MISfang@users.noreply.github.com>
2021-05-10 12:42:18 +0800

    练习一下如何提交到github上

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,22 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+
+    <!-- 路由站位符 -->
     <router-view/>
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  name:'app',
+  data(){
+    return{
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
     }
   }
 }
+</script>
+
+<style lang="less">
+
 </style>
